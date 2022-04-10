1fa1bc765e00418619209ede4cb8324056b02899
Bill <33042795+heyitsbill@users.noreply.github.com>
2022-04-10 07:22:56 -0400

    colors

--- client/screens/UserPostScreen.tsx
+++ client/screens/UserPostScreen.tsx
@@ -88,34 +88,17 @@ export default function UserPostScreen({ navigation }: any, props: any) {
   )
 }
 
-
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    title: {
-      fontSize: 20,
-      fontWeight: 'bold',
-    },
-    separator: {
-      marginVertical: 30,
-      height: 1,
-      width: '80%',
-    },
-  });
   
   const menu = StyleSheet.create({
       wrapper: {
         flex: 1,
         flexDirection: 'column',
-        backgroundColor: '#8aebff',
+        backgroundColor: '#DAF7A6',
       },
     
       upperContainer: {
         borderColor: '#fff',
-        borderWidth: 5,
+        borderWidth: 2,
         borderRadius: 15,
         flex: 13,
       },
@@ -127,122 +110,6 @@ const styles = StyleSheet.create({
       },
     })
     
-  const item = StyleSheet.create({
-      card: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 10,
-        height: 120,
-        elevation: 3,
-        backgroundColor: '#fff',
-        borderRadius: 6,
-        shadowRadius: 2,
-        margin: 4,
-        marginVertical: 8,
-      },
-    
-      leftSide: {
-        flex: 6,
-      },
-    
-      itemName: {
-        fontFamily: 'HindSiliguri-Bold',
-        fontWeight: 'bold',
-        color: '#222',
-        fontSize: 20,
-        marginBottom: 5,
-      },
-    
-      itemDescription: {
-        fontFamily: 'Roboto-Italic',
-        color: '#777',
-        fontSize: 11,
-        flex: 2,
-        marginRight: 10,
-      },
-    
-      itemPrice: {
-        fontFamily: 'HindSiliguri',
-        fontWeight: 'bold',
-        color: '#222',
-        fontSize: 18,
-        //backgroundColor: 'blue'
-      },
-    
-      button: {
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 0 },
-        shadowOpacity: 0.3,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        textAlign: 'center',
-        borderRadius: 10,
-        height: '38%',
-        alignItems: 'center',
-        width: '10%',
-      },
-    
-      buttonText: {
-        justifyContent: 'center',
-        fontFamily: 'Roboto',
-        alignItems: 'center',
-        fontSize: 25,
-        textAlignVertical: 'center',
-      },
-    
-      countText: {
-        fontFamily: 'Roboto',
-        fontWeight: 'bold',
-        fontSize: 24,
-        textAlign: 'center',
-      },
-    
-      spacer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-    
-      buttonSpacer: {
-        flex: 1,
-        //justifyContent: 'center',
-        //alignItems: 'center',
-      },
-    
-      outerContainer: {
-        flex: 2,
-        backgroundColor: '#fff',
-      },
-    
-      upperContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        backgroundColor: '#eee',
-        alignItems: 'center',
-        justifyContent: 'space-evenly',
-      },
-    
-      lowerContainer: {
-        flexDirection: 'row',
-        flex: 2,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-    
-      priceText: {
-        fontFamily: 'HindSiliguri',
-        color: '#000',
-        fontSize: 18,
-        padding: 10,
-      },
-    
-      checkoutText: {
-        fontFamily: 'HindSiliguri-Bold',
-        color: '#fff',
-        fontSize: 25,
-      },
-    })
   
   const home = StyleSheet.create({
       app: {
@@ -274,7 +141,7 @@ const styles = StyleSheet.create({
     
       menuView: {
         margin: 8,
-        backgroundColor: "#8aebff",
+        backgroundColor: "#DAF7A6",
       },
     
       footer: {
