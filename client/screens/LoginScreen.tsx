e00883db67abc927ba417f15c58c598d8a322fe8
Bill <33042795+heyitsbill@users.noreply.github.com>
2022-04-09 21:21:53 -0400

    save

---
+++ client/screens/LoginScreen.tsx
@@ -0,0 +1,9 @@
+import { Text, View, Pressable, ScrollView, ActivityIndicator } from 'react-native'
+
+export default LoginScreen = () => {
+  return (
+    <View>
+      <Text>LoginScreen</Text>
+    </View>
+  );
+};
